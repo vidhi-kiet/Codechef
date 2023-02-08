7ea876d9a03af88257d7d9760071c50e2bd3c997
vidhi <78697483+vidhi-kiet@users.noreply.github.com>
2023-02-08 22:00:30 +0530

    Add files via upload

---
+++ TYRE.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+
+using namespace std;
+
+int main() {
+	   int t;
+	   cin>>t;
+	   while(t--)
+	   {
+	       int n,m;
+	       cin>>n>>m;
+	       int a=2*n+4*m;
+	       cout<<a<<endl;
+	       
+	   }
+	return 0;
+}
