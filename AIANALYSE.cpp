96e4807c8707513fd4f39823fd7c5f32526ac99d
vidhi <78697483+vidhi-kiet@users.noreply.github.com>
2023-02-08 21:57:43 +0530

    Add files via upload

---
+++ AIANALYSE.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+
+
+using namespace std;
+
+int main() {
+	   
+	    int x;
+	    cin>>x;
+	    if(x<=1000)
+	    {
+	        cout<<"yes"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"no"<<endl;
+	    }
+	              
+	       
+	   
+	return 0;
+}
